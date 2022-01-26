f064832caf3e62bec32db7429546ac2988e77e8e
sa1 <me@sa1.co.in>
2022-01-26 16:13:51 +0530

    Updated the chakra theme with few tweaks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import { extendTheme } from "@chakra-ui/react"
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+import { theme as chakraTheme } from "@chakra-ui/react"
 import "../styles/global.css"
 import "../styles/header.css"
 
 const theme = extendTheme({
   fonts: {
+    ...chakraTheme.fonts,
     heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
     body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
   },
+  config: {
+    ...chakraTheme.config,
+    initialColorMode: "light",
+    useSystemColorMode: true,
+  },
+  breakpoints: createBreakpoints({
+    sm: "50em",
+    md: "65em",
+    lg: "90em",
+    xl: "100em",
+  }),
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
